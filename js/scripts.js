423fea6add140cc3baf93de3ba197248c7bc1e98
Andrew Giang <giang184@gmail.com>
2021-06-01 14:30:02 -0700

    add buttons for week 2 project

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  $("button#light").click(function() {
+    $("body").removeClass();
+    $("body").addClass("light-background");
+  });
+
+  $("button#dark").click(function() {
+    $("body").removeClass();
+    $("body").addClass("dark-background");
+  });
+
+  $("button#original").click(function() {
+    $("body").removeClass();
+  });
+
+});
